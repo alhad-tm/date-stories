e4940616489c059e153b45a21f201c93f9888045
alhad-tm <alhadchikkutm@gmail.com>
2025-01-22 23:55:26 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span className='text-5xl text-red-300'>
+        Date Stories
+      </span>
+    </div>
+  );
+}
+
+export default App;
