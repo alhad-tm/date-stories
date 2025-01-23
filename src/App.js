47b0a33228a1a119bc9604f3ec712fc21c92a266
alhad-tm <alhadchikkutm@gmail.com>
2025-01-23 11:12:34 +0530

    displayed today's story

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Stories from './components/Stories/Stories';
 function App() {
   return (
     <div className="App">
-      <span className='text-5xl text-red-300'>
-        Date Stories
-      </span>
+      
       <Stories/>
     </div>
   );
--- src/components/Stories/Stories.jsx
+++ src/components/Stories/Stories.jsx
@@ -1,12 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Stories = () => {
+   const [story,setStory]=useState("")
+
+
+    const date= new Date();
+    const day=date.getDate();
+    const month=date.getMonth()+1;
+    console.log(day,"hi day");
+    console.log(month,"hi month");
+    
   useEffect(() => {
     const getStories = async () => {
       try {
-        const response = await axios.get("http://numbersapi.com/2/2/date");
-        console.log(response.data);
+        const response = await axios.get(`http://numbersapi.com/${month}/${day}/date`);
+        console.log(response.data,"this");
+        setStory(response.data)
       } catch (error) {
         console.log(error);
       }
@@ -14,8 +24,14 @@ const Stories = () => {
     getStories();
   }, []);
 
-  return;
-  <div></div>;
+  return(
+  <div className="bg-blue-600 flex flex-col items-center justify-center gap-12 min-h-screen w-full">
+    <span className='text-5xl text-red-300'>
+        Date Stories
+      </span>
+  <span className="text-3xl text-yellow-100 ">   {story || "loading"} </span>
+  </div>
+  )
 };
 
 export default Stories;
